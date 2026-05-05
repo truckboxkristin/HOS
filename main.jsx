17feeff314d32bf0082ce372bc72660435f19a41
truckboxkristin <kristin@sebastianmoving.net>
2026-05-05 14:53:16 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import DriverHOSTracker from "./App.jsx";
+import DriverHOSTracker from "./driver_hos_tracker_web_app.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
